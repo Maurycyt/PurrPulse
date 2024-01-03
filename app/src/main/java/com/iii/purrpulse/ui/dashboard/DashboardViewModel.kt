d4339deaea869a012700ea4f904ee73b7d6ded0a
Maurycyt <ma.wojda3@student.uw.edu.pl>
2024-01-03 18:50:05 +0100

    Small changes

--- app/src/main/java/com/iii/purrpulse/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/iii/purrpulse/ui/dashboard/DashboardViewModel.kt
@@ -3,11 +3,29 @@ package com.iii.purrpulse.ui.dashboard
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.delay
 
 class DashboardViewModel : ViewModel() {
+    private val _text = MutableLiveData("This is the dashboard Fragment.")
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+    init {
+        startCoroutine()
     }
+
+    private fun startCoroutine() {
+        GlobalScope.launch(Dispatchers.Default) {
+            var counter = 0
+            while (true) {
+                delay(1000)
+                counter++
+                println("Updating text with $counter.")
+                _text.postValue("This is the dashboard Fragment ($counter)")
+            }
+        }
+    }
+
     val text: LiveData<String> = _text
 }

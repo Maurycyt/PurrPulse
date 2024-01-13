15ee1f9f7c51a96652406ed628c76524a9294551
Antoni <a.puch@student.uw.edu.pl>
2024-01-13 11:40:33 +0100

    WIP

--- app/src/main/java/com/iii/purrpulse/ui/home/HomeFragment.kt
+++ app/src/main/java/com/iii/purrpulse/ui/home/HomeFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import com.iii.purrpulse.R
 import com.iii.purrpulse.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -17,22 +21,37 @@ class HomeFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private var homeViewModel : HomeViewModel? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
+        homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
+        homeViewModel!!.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        view.findViewById<Button>(R.id.test_button).setOnClickListener {
+            // create a Toast with some text, to appear for a short time
+            val myToast = Toast.makeText(context, "Hello Toast!", Toast.LENGTH_SHORT)
+            // show the Toast
+            myToast.show()
+
+
+            view.findNavController().navigate(R.id.action_home_to_gamelist)
+        }
+
     }
 
     override fun onDestroyView() {

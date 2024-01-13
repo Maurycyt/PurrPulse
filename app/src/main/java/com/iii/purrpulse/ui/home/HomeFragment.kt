2c3cdc00d6f4faa5be9a5c145765d6b6d83cd92a
AntoniPuch <107212146+AntoniPuch@users.noreply.github.com>
2024-01-13 18:00:34 +0100

    Basic Navigation (#2)
    * Changed page names
    
    * WIP
    
    * WIP
    
    * Added basic navigation and gamelist screen
    
    * Made it all look nicer
    
    * Fixed the issue with the section title being changed only after moving to the section, resulting in the old title being displayed for a short period

--- app/src/main/java/com/iii/purrpulse/ui/home/HomeFragment.kt
+++ app/src/main/java/com/iii/purrpulse/ui/home/HomeFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import com.iii.purrpulse.R
 import com.iii.purrpulse.databinding.FragmentHomeBinding
+import com.iii.purrpulse.ui.gamelist.GamelistFragment
 
 class HomeFragment : Fragment() {
 
@@ -17,22 +20,41 @@ class HomeFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private var homeViewModel : HomeViewModel? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
+        homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
+        /*val textView: TextView = binding.textHome
+        homeViewModel!!.text.observe(viewLifecycleOwner) {
             textView.text = it
-        }
+        }*/
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        view.findViewById<Button>(R.id.easy_games).setOnClickListener {
+            GamelistFragment.setEasyGames()
+            view.findNavController().navigate(R.id.action_home_to_gamelist)
+        }
+
+        view.findViewById<Button>(R.id.mid_games).setOnClickListener {
+            GamelistFragment.setMidGames()
+            view.findNavController().navigate(R.id.action_home_to_gamelist)
+        }
+
+        view.findViewById<Button>(R.id.hard_games).setOnClickListener {
+            GamelistFragment.setHardGames()
+            view.findNavController().navigate(R.id.action_home_to_gamelist)
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/iii/purrpulse/ui/shop/ShopFragment.kt
+++ app/src/main/java/com/iii/purrpulse/ui/shop/ShopFragment.kt
@@ -1,4 +1,4 @@
-package com.iii.purrpulse.ui.notifications
+package com.iii.purrpulse.ui.shop
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.iii.purrpulse.databinding.FragmentNotificationsBinding
+import com.iii.purrpulse.databinding.FragmentShopBinding
 
-class NotificationsFragment : Fragment() {
+class ShopFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentShopBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val shopViewModel =
+            ViewModelProvider(this).get(ShopViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentShopBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textShop
+        shopViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
